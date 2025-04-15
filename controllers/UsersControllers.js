9a69ec00c43cba7ad076fe5fa4beaf3c1f3ec9ac
Giovani Santos <jiovanisantos9@gmail.com>
2025-04-15 18:53:42 -0100

    feat(user): adicionar verificacoes e hashing de senha

--- controllers/UsersControllers.js
+++ controllers/UsersControllers.js
@@ -1,9 +1,22 @@
 const db = require("../config/db")
+const bcrypt = require('bcrypt');
 
 module.exports = {
     async create(req, res) {
         const { nome, sobrenome, email, password } = req.body;
-        const newUser = await db.models.User.create({ nome, sobrenome, email, password });
+
+        if (!nome || !sobrenome || !email || !password) {
+            return res.status(400).json({ error: "Todos os campos são obrigatórios." });
+          }
+
+        const existingUser = await db.models.User.findOne({ where: { email } });
+        if (existingUser) {
+            return res.status(409).json({ error: 'E-mail já cadastrado' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await db.models.User.create({ nome, sobrenome, email, password:hashedPassword });
         res.status(201).json(newUser);
       },
 
--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 require("dotenv").config()
+const cors = require("cors");
 const express = require('express')
 const db = require('./config/db');
 const usersRoutes = require('./routes/usersRoutes');
 
 const app = express()
+
+app.use(cors());
 app.use(express.json());
 app.use('/users', usersRoutes);
 
 const port = process.env.PORT ?? 5000
 
-db.connection.sync().then(() => {
+db.connection.sync({ force: false }).then(() => {
   app.listen(port, () => {
     console.log(`App listening on port ${port}`)
   })
